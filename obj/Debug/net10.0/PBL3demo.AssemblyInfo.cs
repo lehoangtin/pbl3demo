4b9a01b866b2a6fa82cf11952987a1f8a51010bd
le hoang tin <hoangtin422006@gmail.com>
2026-04-11 14:33:38 +0700

    Updatetieptuc

--- obj/Debug/net10.0/PBL3demo.AssemblyInfo.cs
+++ obj/Debug/net10.0/PBL3demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PBL3demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc51224b272e13c3a177f45eb3f51b5d0a511c26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b959466a6a7e42218ae7bcb57724d3dcff5a8d98")]
 [assembly: System.Reflection.AssemblyProductAttribute("PBL3demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PBL3demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
